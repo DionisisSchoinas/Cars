8b968712a3145f402b1f8cbc8fc04c3c08456b42
DionisisSchoinas <p17134@students.cs.unipi.gr>
2018-03-01 19:21:07 +0200

    Created module
    Moved input to Input module

--- Cars/Cars/Cars.py
+++ Cars/Cars/Cars.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 import time
+import Input
 clear = lambda: os.system('cls')
 
 
@@ -11,51 +12,10 @@ session_requests = requests.session()
 login_url = "https://www.car.gr/login/"
 result = session_requests.post(login_url , data = payload , headers = dict(ReferenceError=login_url))
 
-
-# Get info from user
-pricing = int(raw_input("Give max price for car : \n"))
-filters = {1:"&condition=%CE%9A%CE%B1%CE%B9%CE%BD%CE%BF%CF%8D%CF%81%CE%B9%CE%BF",
-           2:"&condition=%CE%9C%CE%B5%CF%84%CE%B1%CF%87%CE%B5%CE%B9%CF%81%CE%B9%CF%83%CE%BC%CE%AD%CE%BD%CE%BF",
-           3:"&offer_type=sale",
-           4:"&offer_type=wanted",
-           5:"&offer_type=rent"}
-go = True
-f = []
-fa = []
-while True:
-    if len(f)==5:
-        print "\n- All filters are selected -"
-        break
-    clear()
-    print "Give filters :"
-    print "  1 - kainourgio"
-    print "  2 - metaxeirismeno"
-    print "  3 - pwleitai"
-    print "  4 - zhteitai"
-    print "  5 - enoikiazetai"
-    if len(f)==0:
-        print "  6 - no filter"
-    i = raw_input()
-    if i in fa:
-        print " Give filter's code only once"
-        time.sleep(2)
-        continue
-    if i=="6":
-        break
-    try:
-        f.append(str(filters[int(i)]))
-        fa.append(i)
-    except:
-        print "Input must be : 1 or 2 or 3 or 4 or 5"
-        time.sleep(2)
-        continue
-    cont = raw_input("Add more filters ? ( yes / no )\n")
-    if cont[0]=="Y" or cont[0]=="y":
-        continue
-    elif cont[0]=="N" or cont[0]=="n":
-        break
-filter = ''.join(f)
-
+# Getting info with Input module
+f = Input.Get_Info()
+filter = ''.join(f[0])
+pricing = f[1]
 
 # Get results
 def Get_Results(page):
---
+++ Cars/Cars/Input.py
@@ -0,0 +1,69 @@
+import os
+import time
+clear = lambda: os.system('cls')
+# Get info from user
+def Get_Info():
+    while True:
+        price = raw_input("Give max price for car : \n")
+        if price ==" ":
+            print " You have to give something"
+            continue
+        try:
+            pricing = int(price)
+        except:
+            print " Give an integer"
+            continue
+        filters = {1:"&condition=%CE%9A%CE%B1%CE%B9%CE%BD%CE%BF%CF%8D%CF%81%CE%B9%CE%BF",
+                   2:"&condition=%CE%9C%CE%B5%CF%84%CE%B1%CF%87%CE%B5%CE%B9%CF%81%CE%B9%CF%83%CE%BC%CE%AD%CE%BD%CE%BF",
+                   3:"&offer_type=sale",
+                   4:"&offer_type=wanted",
+                   5:"&offer_type=rent"}
+        f = []
+        fa = []
+        while True:
+            if len(f)==5:
+                print "\n- All filters are selected -"
+                break
+            clear()
+            print "You have "+str(len(f))+" filters"
+            print "Filters :"
+            print "  1 - kainourgio"
+            print "  2 - metaxeirismeno"
+            print "  3 - pwleitai"
+            print "  4 - zhteitai"
+            print "  5 - enoikiazetai"
+            if len(f)==0:
+                print "  6 - no filter"
+            else:
+                print "  6 - finished with the filters"
+            while True:
+                i = raw_input()
+                if i==" ":
+                    print " You have to give something"
+                else:
+                    break
+            if i in fa:
+                print " Give filter's code only once"
+                time.sleep(2)
+                continue
+            if i=="6":
+                break
+            try:
+                f.append(str(filters[int(i)]))
+                fa.append(i)
+            except:
+                print "Input must be : 1 or 2 or 3 or 4 or 5"
+                time.sleep(2)
+                continue 
+            while True:
+                cont = raw_input("Add more filters ? ( yes / no )\n")
+                if cont==" ":
+                    print " You have to give something"
+                else:
+                    break
+            if cont[0]=="Y" or cont[0]=="y":
+                continue
+            elif cont[0]=="N" or cont[0]=="n":
+                break
+        break
+    return [f,pricing]
