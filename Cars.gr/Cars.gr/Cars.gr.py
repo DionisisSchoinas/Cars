18706d3752fb0bae1c226091795df02680dc37cd
DionisisSchoinas <p17134@students.cs.unipi.gr>
2018-03-09 22:53:27 +0200

    Changed functions

--- Cars.gr/Cars.gr/Cars.gr.py
+++ Cars.gr/Cars.gr/Cars.gr.py
@@ -1,23 +1,33 @@
 import Tkinter as tk
 import os
+import Data
 
-def write_slogan():
-    print("Tkinter is easy to use!")
-def call():
-	os.system('python Data.py')
+filters = []
+def filter():
+    filters = Data.Make_Filters()
+    print filters
+def Start_search():
+    print filters
+    Data.Search(filters)
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
-button = tk.Button(frame, 
+button1 = tk.Button(frame, 
                    text="QUIT", 
                    fg="red",
                    command=quit)
-button.pack(side=tk.LEFT)
-slogan = tk.Button(frame,
-                   text="Search",
-                   command=call)
-slogan.pack(side=tk.LEFT)
+button1.pack(side=tk.LEFT)
+
+button2 = tk.Button(frame,
+                   text="Give Filters",
+                   command=filter)
+button2.pack(side=tk.RIGHT)
+
+button3 = tk.Button(frame,
+                    text="Start Searching",
+                    command=Start_search)
+button3.pack(side=tk.RIGHT)
 
 root.mainloop()
 
